983844b9e0e9ede047e777b482f70e262c338fe1
Loralee Dawn <loraleedawn93@gmail.com>
2019-10-19 13:26:15 -0700

    working on the form

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,2 @@
-var APIkey = LyoHtWAdocofJU4Hx7Y10T0jIN8Igav2;
-
-// Example APIkey: 
-// https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=yourkey
+// Example APIkey: LyoHtWAdocofJU4Hx7Y10T0jIN8Igav2;
+// https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=LyoHtWAdocofJU4Hx7Y10T0jIN8Igav2;
