79913e517ff059b222c87d5d17dc58efe98ebc2c
Nadine Ju <nadine.jura@online.de>
2019-10-19 13:28:29 -0700

    added jQuery and linked app.js

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,17 @@
+/***********************************
+ VARIABLES
+***********************************/
+
 var APIkey = LyoHtWAdocofJU4Hx7Y10T0jIN8Igav2;
 
 // Example APIkey: 
 // https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=yourkey
+
+
+/***********************************
+ FUNCTIONS
+***********************************/
+
+/***********************************
+ CODE
+***********************************/
