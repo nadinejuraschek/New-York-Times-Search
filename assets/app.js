3c8458ec0f8566a35e36665301b247484e8977b2
Nadine Ju <nadine.jura@online.de>
2019-10-21 14:59:53 -0700

    Pseudocode and starter variables added

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,16 @@
 /***********************************
  VARIABLES
 ***********************************/
+const APIkey = "LyoHtWAdocofJU4Hx7Y10T0jIN8Igav2";
 
-var APIkey = "LyoHtWAdocofJU4Hx7Y10T0jIN8Igav2";
+var searchTerm = "";
 
-// Example APIkey: 
-// https://api.nytimes.com/svc/search/v2/articlesearch.json?q= + searchTerm + &api-key=LyoHtWAdocofJU4Hx7Y10T0jIN8Igav2;
+var recordNumber = 0;
 
+var endYear = 0;
+var startYear = 0;
+
+var queryURL = "";
 
 /***********************************
  FUNCTIONS
@@ -16,8 +20,27 @@ var APIkey = "LyoHtWAdocofJU4Hx7Y10T0jIN8Igav2";
  EVENTS
 ***********************************/
 
-$("#search").
+$("#search-btn").on("click", function (event) {
+
+});
+
+$("#clear-btn").on("click", function (event) {
+
+});
 
 /***********************************
- CODE
+ MAIN CODE
+
+ // Example APIkey:
+ // https://api.nytimes.com/svc/search/v2/articlesearch.json?q= + searchTerm + &api-key=LyoHtWAdocofJU4Hx7Y10T0jIN8Igav2;
+
 ***********************************/
+// get and store user inputs
+
+// create queryURL with user inputs
+
+// ajax call for search button
+
+// display articles
+
+// set up clear button
