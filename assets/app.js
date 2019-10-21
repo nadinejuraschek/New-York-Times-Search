a092558fb3f934c5ea5d4cdf5cc8bd313ff31d53
Nadine Ju <nadine.jura@online.de>
2019-10-21 14:52:23 -0700

    added ids to buttons in inputs for later code

--- assets/app.js
+++ assets/app.js
@@ -2,15 +2,21 @@
  VARIABLES
 ***********************************/
 
-var APIkey = LyoHtWAdocofJU4Hx7Y10T0jIN8Igav2;
+var APIkey = "LyoHtWAdocofJU4Hx7Y10T0jIN8Igav2";
 
 // Example APIkey: 
-// https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=LyoHtWAdocofJU4Hx7Y10T0jIN8Igav2;
+// https://api.nytimes.com/svc/search/v2/articlesearch.json?q= + searchTerm + &api-key=LyoHtWAdocofJU4Hx7Y10T0jIN8Igav2;
 
 
 /***********************************
  FUNCTIONS
 ***********************************/
+
+/***********************************
+ EVENTS
+***********************************/
+
+$("#search").
 
 /***********************************
  CODE
